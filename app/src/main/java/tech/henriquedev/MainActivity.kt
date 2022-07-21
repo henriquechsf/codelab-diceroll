d9577cf19a2bd7b0d178c4b259206973cd8c1519
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-07-20 22:00:23 -0300

    Create an interactive Dice Roller app

--- app/src/main/java/tech/henriquedev/MainActivity.kt
+++ app/src/main/java/tech/henriquedev/MainActivity.kt
@@ -2,10 +2,25 @@ package tech.henriquedev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.btn_roll)
+
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        val resultText = findViewById<TextView>(R.id.text_result)
+        resultText.text = diceRoll.toString()
     }
 }
