6234008723d8a03e616375fb7b4e036a8a46a28e
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-07-20 23:10:45 -0300

    logging examples

--- app/src/main/java/tech/henriquedev/MainActivity.kt
+++ app/src/main/java/tech/henriquedev/MainActivity.kt
@@ -2,12 +2,15 @@ package tech.henriquedev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener { rollDice() }
 
         rollDice()
+        logging()
     }
 
     /**
@@ -44,5 +48,13 @@ class MainActivity : AppCompatActivity() {
         diceImage.setImageResource(drawableResource)
         // Update the content description
         diceImage.contentDescription = diceRoll.toString()
+    }
+
+    private fun logging() {
+        Log.e(TAG, "ERROR: a serious error like an app crash")
+        Log.w(TAG, "WARN: warns about the potential for serious errors")
+        Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+        Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+        Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
     }
 }
